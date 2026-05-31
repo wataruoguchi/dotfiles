712f2c5f99e7ef27644d50766f1d99920a3c1aae
wataru <wataru.oguchi.0to1@gmail.com>
2026-05-30 22:18:09 -0700

    chore: add tmux-ish key binds

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -16,6 +17,53 @@ config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
+
+-- Leader key (Ctrl+a, like tmux default prefix but use 'a' instead of 'b')
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.keys = {
+	-- Pane splitting
+	-- leader + " = split top/bottom (horizontal divider)
+	{ key = '"', mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	-- leader + % = split left/right (vertical divider)
+	{ key = "%", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+	-- Pane navigation (vim-style)
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+
+	-- Pane navigation (arrow keys)
+	{ key = "LeftArrow", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "DownArrow", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "UpArrow", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "RightArrow", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+
+	-- Pane resizing (leader + Shift + vim keys)
+	{ key = "H", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) },
+
+	-- Pane management
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+
+	-- Scroll / copy mode (like tmux prefix + [)
+	-- In copy mode: hjkl/arrows to move, v to select, y to copy, q to quit
+	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+
+	-- Search (like tmux prefix + /)
+	-- After opening: type to search, Enter/n to go to next, N for previous, Esc to close
+	{ key = "/", mods = "LEADER", action = act.Search("CurrentSelectionOrEmptyString") },
+
+	-- Quick scroll without entering copy mode
+	{ key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-1) },
+	{ key = "PageDown", mods = "SHIFT", action = act.ScrollByPage(1) },
+	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-3) },
+	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(3) },
+}
 
 -- and finally, return the configuration to wezterm
 return config
