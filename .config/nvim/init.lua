7f63f892345bce0c1431d474b7d1ff62e0807941
wataru <wataru.oguchi.0to1@gmail.com>
2026-05-31 16:53:58 -0700

    fix: avoid conflicts with agents

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,15 @@
 require("wataru.core")
 require("wataru.lazy")
 require("wataru.lsp")
+
+-- Reload files changed on disk automatically
+vim.opt.autoread = true
+
+-- Trigger the check to focus/buffer events and cursor idle
+vim.api.nvim_create_autocmd(
+  { "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" },
+  { pattern = "*", command = "checktime" }
+)
+
+-- Fire CursorHold faster (default is 4000ms - too slow)
+vim.opt.updatetime = 500
--- .config/nvim/lua/wataru/plugins/treesitter.lua
+++ .config/nvim/lua/wataru/plugins/treesitter.lua
@@ -1,55 +1,41 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  tag = "v0.10.0",
-  lazy = false,
   build = ":TSUpdate",
   event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
+  main = "nvim-treesitter.configs",
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    -- c, lua, vim, vimdoc, query are bundled with Neovim 0.10+
+    ensure_installed = {
+      "json",
+      "javascript",
+      "typescript",
+      "tsx",
+      "yaml",
+      "html",
+      "css",
+      "prisma",
+      "markdown",
+      "markdown_inline",
+      "svelte",
+      "graphql",
+      "bash",
+      "dockerfile",
+      "gitignore",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
       },
-      -- enable indentation
-      indent = { enable = true },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-
-    -- use bash parser for zsh files
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
     vim.treesitter.language.register("bash", "zsh")
   end,
 }
