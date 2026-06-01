ad3268ea4c1ef572714180a7ea324cc0907b7944
wataru <wataru.oguchi.0to1@gmail.com>
2026-05-31 21:10:03 -0700

    chore: styling tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,17 +6,55 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-
+config.automatically_reload_config = true
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 15
 
-config.enable_tab_bar = false
-
-config.window_decorations = "RESIZE"
-
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
+
+-- Tab
+
+config.window_decorations = "RESIZE"
+config.show_tabs_in_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.window_frame = {
+	inactive_ttilebar_bg = "none",
+	active_ttilebar_bg = "none",
+}
+config.window_background_gradient = {
+	colors = { "#000000" },
+}
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+config.colors = {
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
+}
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+	local edge_background = "none"
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+	local edge_foreground = background
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
 
 -- Leader key (Ctrl+a, like tmux default prefix but use 'a' instead of 'b')
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
